2130bf3318c46ab4f168200da681f694a82c0d0c
Srijeeta Maity <srijeeta.dona@gmail.com>
2020-06-10 22:18:33 +0530

    Add files via upload

---
+++ grid/index_grid.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- header section -->
+<?php include $_SERVER["DOCUMENT_ROOT"] .'/header.php';?>
+
+<body style="padding: 50px;">
+	
+
+	<div class="container text-center rounded_rectangle">
+		<div class="row" style="padding:15px">
+			<!-- <div class="col-md-12 col-sm-12"> -->
+			<div class="hero-unit">
+				<h1><span>Smart Grid</span></h1>
+			</div>
+			<!-- </div> -->
+		</div>
+		<div class="row" style="padding:15px">
+			<img class="img-responsive" src="/grid/images/Grid.jpg" alt="cpsbanner">
+		</div>
+		<div class="row topic-desc lead" style="padding:15px">
+			<p><span>
+				</span></p>
+		</div>
+		<br>
+			<hr class="divider-line">
+	</div>
+
+
+<!-- divider section -->
+
+<div class="divider">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-sm-6">
+				<div class="divider-wrapper divider-one">
+					<i class="fa fa-laptop"></i>
+					<h2>Smart Grid Design Tool</h2>
+					<p></p>
+					<a href="/grid/smartgrid_Tool.php" class="btn btn-default">LEARN MORE</a>
+					<p> </p>
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-6">
+				<div class="divider-wrapper divider-two">
+					<i class="fa fa-laptop"></i>
+					<h2>Intelligent Building Energy system</h2>
+					<p> </p>
+					<a href="/grid/BEMS.php" class="btn btn-default">LEARN MORE</a>
+					<p> </p>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</div>
+<div class="divider">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-sm-6">
+				<div class="divider-wrapper divider-two">
+					<i class="fa fa-laptop"></i>
+					<h2>Smart Grid Privacy</h2>
+					<p> </p>
+					<a href="/grid/Privacy.php" class="btn btn-default">LEARN MORE</a>
+					<p> </p>
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-6">
+				<div class="divider-wrapper divider-one">
+					<i class="fa fa-laptop"></i>
+					<h2>CAD for HVAC Automation</h2>
+					<p> </p>
+					<a href="/grid/HVAC.php" class="btn btn-default">LEARN MORE</a>
+					<p> </p>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</div>
+
+<!-- about section
+<div id="about" style="padding: 50px;">
+	<div class="container" style="padding: 20px;" >
+		<div class="row">
+			<div class="col-md-4 col-sm-4" >
+				<img src="/grid/images/Resources.jpg" class="img-responsive" alt="about img" style="padding-top: 50px;">
+			</div>
+			<div class="col-md-8 col-sm-12 about-des">
+				<h2>Resources</h2>
+				<p></p>
+				<p></p>
+				<a href="/grid/about.php" class="btn btn-default">LEARN MORE</a>
+
+			</div>
+		</div>
+	</div>
+</div>
+-->
+	
+
+<!-- footer section -->
+<?php include $_SERVER["DOCUMENT_ROOT"] .'/footer.php';?>
+
+
+</body>
+</html>
